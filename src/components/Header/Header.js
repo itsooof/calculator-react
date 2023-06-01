9e14b477b08921bede6aaf2cd4713c3691374d1a
Kavya Paliwal <kpaliwal2002@gmail.com>
2023-06-01 13:58:53 +0530

    Files upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from 'react';
+import './Header.css';
+
+function Header(props) {
+  const resultRef=useRef();
+  const expressionRef=useRef();
+
+  useEffect(()=>{
+    resultRef.current.scrollIntoView({behaviour:"smooth"})
+  },[props.history]);
+
+  useEffect(()=>{
+    expressionRef.current.scrollLeft=expressionRef.current.scrollWidth;
+  },[props.expression]);
+
+  return (
+    <div className='header custom-scroll'>
+      <div className="header_history">
+        {props.history &&
+          
+          props.history?.map((item,index)=>(<p key={item+Math.random()*44}>{item}</p>))
+        }
+        
+      </div>
+      <br />
+      <div ref={expressionRef} className='header_expression custom-scroll'>
+        <p>{props.expression}</p>
+      </div>
+      <br />
+      <p ref={resultRef} className='header_result'>{props.result}</p>
+    </div>
+  );
+}
+
+export default Header;
